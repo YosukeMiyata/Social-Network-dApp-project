33602fc096acd83cd951ce16bd389f50f32f13f5
YosukeMiyata <49312744+YosukeMiyata@users.noreply.github.com>
2022-10-27 11:57:01 +0900

    Add files via upload

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -5,7 +5,14 @@ pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
 
+// 最初にOpenZeppelinライブラリをインポートします.
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 contract SocialNetwork {
+
+    // OpenZeppelinによりtokenIdsの追跡が容易になります。
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     //新しいコメントが投稿された時のイベント、
     //コメントのインデックス、投稿者のウォレットアドレス、コメント、投稿日時、ライク数をフロントに渡す。
@@ -15,6 +22,8 @@ contract SocialNetwork {
     //ライクボタンが押された時のイベント、
     //ライクボタンが押されたコメントのインデックスと新しいライク数をフロントに渡す。
     event NewLike(uint256 id, uint256 totallikes, bool flagCompleted);
+
+    event Transfer(address _from, address _to, uint256 _value);
     
     //コメントに関する構造体
     struct Comment {
@@ -25,111 +34,45 @@ contract SocialNetwork {
         uint256 totallikes; // コメントに付いたライク数
     }
     
-    //任意のコメントにライクを押したユーザーのアドレスを格納する構造体
-    struct Like {
-        address[] uniqueUser;
+    struct Favorite {
+        mapping(address => bool) isFavorite;
+        // address毎に既にいいねしているかのbooleanを紐づける
     }
 
-    //2の32乗
-    uint256 constant twoToThe32thPower = 4294967295;
-    //2の256乗-1
-    uint256 constant twoToThe256thPower = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
-    //構造体Likeの中に任意の配列が存在するかどうかを示すbool変数を格納する配列
-    //初期値はfalse。falseなら配列はまだ作られてない。trueなら既に配列は存在する
-    bool[ twoToThe32thPower ] private arrayExists; //4294967295はコメント数の上限
-    //構造体Likeの中にあるそれぞれの配列の長さを格納する配列
-    uint256[ twoToThe32thPower ] private numberOfUniqueUser; //twoToThe256thPowerはライク数の上限
+    mapping(uint256 => Favorite) favorites;
+        // 投稿ID毎にFavorite構造体を紐づける
+    mapping(address => uint) balance;
 
     //構造体
     Comment[] comments;
-    //構造体
-    Like[] likes;
+
     //ライク数の初期値
     uint256 constant initialLike = 0;
-    //構造体Commentのインデックス番号
-    uint256 comment_id = 0; 
+
+    //uint256 numnum = 0;
     
-    constructor() {
+    constructor() payable {
         console.log("Social Network!");
     }
 
     //新しいコメントが投稿された時に呼ばれる関数
-    //構造体commentsに諸々の変数を格納して、次のコメントのためにcomment_idに1を足しておく
+    //構造体commentsに諸々の変数を格納して、次のコメントのために_tokenIdsに1を足しておく
     function post(string memory _comment) public {
         
-        //コメント数が適正範囲内かどうか判定
-        require( comment_id < twoToThe32thPower, "The number of comments exceeds the upper limit.");
+        uint256 newRecordId = _tokenIds.current();
 
         console.log("%s posted w/ comment %s", msg.sender, _comment);
         
+        Favorite storage f = favorites[ newRecordId ];
+        f.isFavorite[ msg.sender ] = false;
+        
         //新しいコメントを構造体に格納
-        comments.push( Comment( comment_id, msg.sender, _comment, block.timestamp, initialLike ) );
+        comments.push( Comment( newRecordId, msg.sender, _comment, block.timestamp, initialLike ) );
 
         //フロントに通知
-        emit NewComment( comment_id, msg.sender, _comment, block.timestamp, comments[ comment_id ].totallikes );
+        emit NewComment( newRecordId, msg.sender, _comment, block.timestamp, comments[ newRecordId ].totallikes );
 
-        //indexに1つ足す
-        comment_id++;
-    }
-
-    //ライクを押したユーザーが初めて押したかどうか判定する関数
-    //初めてならtrueを返す。そうでないならfalseを返す。
-    function isFirstLike(uint256 _ind) public returns (bool) {
-        
-        //まずライクされたコメントのインデックス（引数の_indがそれを示す）に対応する
-        //構造体Likeの中の配列が存在するかどうか判定する
-        //arrayExists[_ind]がfalseなら、配列を作るところから始める
-        //trueなら既に配列は存在してるので、配列に格納されてるユニークなアドレスの中に
-        //今回ライクした人のアドレスが含まれているか調べる
-        
-        if( arrayExists[_ind] == false ){ //配列がなければ 
-            
-            //配列を作る
-            likes.push(Like(new address[](0)));
-            //今回ライクした人のアドレスを配列に入れる
-            likes[_ind].uniqueUser.push(msg.sender);
-            //配列の長さを1つ増やす
-            numberOfUniqueUser[_ind] = numberOfUniqueUser[_ind] + 1;
-            console.log(likes[_ind].uniqueUser[0]);
-            //配列は既に作ったことを知らせる
-            arrayExists[_ind] = true;
-            console.log("true");
-            //この人はユニークなユーザーですよという意味でtrueを返す
-            return true;
-        
-        }else{ //既に配列が存在していれば
-            
-            //ユニークユーザーかどうかを調べるためのbool変数
-            bool isUnique = true;
-            //for文でライクが押されたコメントのインデックスに対応する配列の中に
-            //今回ライクを押した人のアドレスと同じものがあるかどうか調べる。
-            //同じものがあれば、isUniqueにfalseを代入。
-            //同じものがなければ、ユニークなアドレスなのでtrueのまま。
-            for(uint i = 0; i < numberOfUniqueUser[_ind]; i++){
-                if( likes[_ind].uniqueUser[i] == msg.sender){
-                    isUnique = false;
-                    break;
-                }
-            }
-
-            if( isUnique ){ //for文を抜けたあとにisUniqueがtrueのままなら
-                //配列に今回ライクした人のアドレスを入れる
-                likes[_ind].uniqueUser.push(msg.sender);
-                console.log(likes[_ind].uniqueUser[ numberOfUniqueUser[_ind] ]);
-                //配列の長さに1を足す
-                numberOfUniqueUser[_ind] = numberOfUniqueUser[_ind] + 1;
-                console.log("OK true");
-                //この人はユニークなユーザーですよという意味でtrueを返す
-                return true;
-            }
-            else{ //for文を抜けたあとにisUniqueがfalseになっていたら
-                console.log("false");
-                //この人は同じコメントにライクをしようとしてますよという意味でfalseを返す
-                return false;
-            }
-        
-        }
-        
+        _tokenIds.increment();
     }
 
     //新しくライクが押された時に呼ばれる関数
@@ -137,26 +80,29 @@ contract SocialNetwork {
     //構造体commentsのtotallikesに1を足して、フロントに通知
     function like(uint256 _num) public {
         
-        //ライク数が適正範囲内かどうか判定
-        //require( numberOfUniqueUser[ _num ] < twoToThe256thPower, "The number of likes exceeds the upper limit.");
-
-        bool isFirst = isFirstLike( _num );
-        console.log("%s likeed w/ at the comment", msg.sender);
+        Favorite storage f = favorites[ _num ];
         
-        if( isFirst ){
+        if( f.isFavorite[ msg.sender ] == false ){
+            
+            f.isFavorite[ msg.sender ] = true;
             
             console.log("total likes is %i", comments[ _num ].totallikes);
             //ライク数に1を足す
             comments[ _num ].totallikes++;
+            
             console.log("total likes is %i", comments[ _num ].totallikes);
 
             //フロントに通知
             emit NewLike( comments[ _num ].id, comments[ _num ].totallikes, true );
         
         }else{
+            
             console.log("Only One Time!");
             emit NewLike( comments[ _num ].id, comments[ _num ].totallikes, false );
+        
         }
+        console.log("%s likeed w/ at the comment", msg.sender);
+
     }
 
     //デバッグ用
@@ -164,10 +110,6 @@ contract SocialNetwork {
     //こっちは同じ人がいくらでもライク数を増やせる
     function likeForDebug(uint256 _num) public {
         
-        
-        //ライク数が適正範囲内かどうか判定
-        require( numberOfUniqueUser[ _num ] < twoToThe256thPower, "The number of likes exceeds the upper limit.");
-
         console.log("total likes is %i", comments[ _num ].totallikes);
         //ライク数に1を足す
         comments[ _num ].totallikes++;
@@ -178,9 +120,53 @@ contract SocialNetwork {
 
     }
 
+    modifier balanceCheck( uint _amount ){
+        require( balance[ msg.sender ] >= _amount, "Insufficient balance" );
+        _;
+    }
+
+    function getBalance() public view returns( uint ){
+        return balance[ msg.sender ];
+    }
+
+    function deposit() public payable {
+        balance[ msg.sender ] += msg.value;
+    }
+
+    function withdraw(uint _amount) public balanceCheck( _amount ) {
+        
+        balance[ msg.sender ] -= _amount;
+        payable( msg.sender ).transfer( _amount );
+    }
+
+    function transfer(uint256 _num, uint256 _value) public returns (bool success) {
+        // 移動したい額のトークンがユーザーのアドレスに存在するか確認
+        // 存在しない場合は、エラーを返す
+        require( _value > 0, "value can't be 0" );
+
+        balance[msg.sender] += _value;
+        // 関数を呼び出したユーザーアドレスから _value の金額を引き抜く
+        balance[msg.sender] -= _value;
+        // 送金先のアドレスに _value の金額を足す
+        balance[ comments[ _num ].poster ] += _value;
+        
+        // Transferイベントを実行する
+        emit Transfer(msg.sender, comments[ _num ].poster, _value);
+        
+        return true;
+    }
+
     //全てのcommentsを返す関数
     function getAllComments() public view returns (Comment[] memory) {
         return comments;
+    }
+
+    function getIsFavorite( uint256 _num ) public view returns ( bool ) {
+        
+        Favorite storage f = favorites[ _num ];
+        
+        return f.isFavorite[ msg.sender ];
+
     }
 
 }
